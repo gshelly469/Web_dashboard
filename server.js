9f4b08737e55207ad59863bbcba57e3334607bcd
Girish <girishshelly469@gmail.com>
2022-01-12 10:37:19 +0530

    changes for the heroku deployment

--- server.js
+++ server.js
@@ -10,5 +10,10 @@ app.use(express.json());
 
 app.use('/getAllUsers', getusers);
 
+// First store env variable in the Settings-->Config Vars
+if (process.env.env === 'Production'){
+    app.use('/client/build')
+}
+
 
 app.listen(process.env.PORT || 5000, ()=> console.log('Backend listening from port 5000'));
